5e44ff1a1ef12700b6ba711c6d6902233b6e0410
Erik Vanderlei Fernandes <erik.v.fernandes@gmail.com>
2023-08-14 23:20:19 -0300

    Adicionando tagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,19 +18,6 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', 'GTM-TMQLFLW7');
-  `,
-          }}
-        />
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       </head>
       <body className={inter.className}>

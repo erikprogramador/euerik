d918830199e4c5bfabd68f5755f4c80192a29783
Erik Vanderlei Fernandes <erik.v.fernandes@gmail.com>
2023-08-14 23:16:40 -0300

    Adicionando tagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Erik V. Fernandes',
-  description: 'Conhecimento e inteligência em negócios',
+  description:
+    'Conhecimento multidisciplinar para aplicação em inteligência de negócios',
 };
 
 export default function RootLayout({
@@ -17,9 +19,33 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', 'GTM-TMQLFLW7');
+  `,
+          }}
+        />
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-TMQLFLW7`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }

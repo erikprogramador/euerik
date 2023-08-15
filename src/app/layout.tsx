ec3071f8af99787672d84e3fbaf2300c374291c0
Erik Vanderlei Fernandes <erik.v.fernandes@gmail.com>
2023-08-14 23:50:32 -0300

    Adicionando tagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import Analytics from '@/components/Analytics';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Suspense } from 'react';
-import GoogleTagManager from '@magicul/next-google-tag-manager';
 import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -26,7 +24,7 @@ export default function RootLayout({
 
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-348548627" />
 
-        {/* <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -34,17 +32,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TMQLFLW7')`,
           }}
-        ></script> */}
+        ></script>
       </head>
       <body className={inter.className}>
-        {/* <GoogleTagManager /> */}
-        {/* <noscript>
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TMQLFLW7"
             height="0"
             width="0"
           ></iframe>
-        </noscript> */}
+        </noscript>
 
         {children}
       </body>
